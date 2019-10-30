ebdf6404c049cbf7ded3e5309e8a06d9a75f47f4
Lev Karpov <nt66@bk.ru>
2019-10-30 14:33:34 +0300

    project 30.10

--- js/basket.js
+++ js/basket.js
@@ -1,27 +1,27 @@
 var basket = document.querySelector(".basket");
-var popup = document.querySelector(".modal-added-cart");
-var close = popup.querySelector(".modal-close");
-var form = popup.querySelector("form");
+var basket_popup = document.querySelector(".modal-added-cart");
+var basket_close = basket_popup.querySelector(".modal-close");
+var basket_form = basket_popup.querySelector("form");
 
 basket.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
+  basket_popup.classList.add("modal-show");
 });
 
-close.addEventListener("click", function (evt) {
+basket_close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal-show");
+  basket_popup.classList.remove("modal-show");
 });
 
-form.addEventListener("submit", function (evt) {
+basket_form.addEventListener("submit", function (evt) {
   evt.preventDefault();
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
+    if (basket_popup.classList.contains("modal-show")) {
+      basket_popup.classList.remove("modal-show");
     }
   }
 });
